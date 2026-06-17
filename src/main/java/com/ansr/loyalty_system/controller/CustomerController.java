8e96951314428ef78c6c35e1ab8b26bd0857177c
Shirleyj25 <johnshirley2922@gmail.com>
2026-06-17 16:23:29 +0530

    Update CORS for deployment

--- src/main/java/com/ansr/loyalty_system/controller/CustomerController.java
+++ src/main/java/com/ansr/loyalty_system/controller/CustomerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import jakarta.validation.Valid;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
